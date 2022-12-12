b8cbfa06facd5d8a7b1ce2cca4b2ee19259bddf5
borkab <borbala.botond@gmail.com>
2022-12-12 11:19:49 +0100

    first

---
+++ app.go
@@ -0,0 +1,17 @@
+package app
+
+import "github.com/gorilla/mux"
+
+type App struct {
+	Router *mux.Router
+}
+
+func New() *App {
+	return &App{
+		Router: mux.NewRouter(),
+	}
+}
+
+func InitRoutes() {
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "postapi/app"
+
+func main() {
+	app.New()
+}
